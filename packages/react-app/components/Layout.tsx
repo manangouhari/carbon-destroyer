f44246f8365e5980388fd4524768bc8698e601e4
manangouhari <gouhari21@hotmail.com>
2023-05-14 18:52:12 +0530

    centered the main screen

--- packages/react-app/components/Layout.tsx
+++ packages/react-app/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout: FC<Props> = ({ children }) => {
     <>
       <div className="bg-snow overflow-hidden flex flex-col min-h-screen">
         <Header />
-        <div className="py-12 max-w-7xl space-y-8 sm:px-14 lg:px-18">
+        <div className="py-12 max-w-7xl space-y-8 sm:px-14 lg:px-18 mx-auto">
           {children}
         </div>
         <Footer />
